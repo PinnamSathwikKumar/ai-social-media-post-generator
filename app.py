a73bbc228c2df17d291d3ec8cb2d6955da88b357
PinnamSathwikKumar <pinnamsathwikkumar@gmail.com>
2026-05-06 19:36:29 +0530

    Railway deployment setup

--- app.py
+++ app.py
@@ -281,5 +281,5 @@ def delete_post(post_id):
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run( host='0.0.0.0', port=5000)
 
